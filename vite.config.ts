0459416a663fe8eed95b84ed0c48c5fc7f74b43a
wmsexton <wmsexton@btinternet.com>
2024-10-14 17:41:15 +0100

    still trying to fix deployed website 17

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000, // specify the port you want Vite to use
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'https://wedding-back-end-ga-32f0d464c773.herokuapp.com',
+        target: 'http://localhost:8000',
         changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
