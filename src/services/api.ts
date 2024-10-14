79db0dc3bdf0a0271e0715e46bb8b7868caafaf9
wmsexton <wmsexton@btinternet.com>
2024-10-14 17:24:39 +0100

    still trying to fix deployed website 15

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'axios';
 import { GuestData } from '../types';
 
 const api = axios.create({
-    baseURL: process.env.VITE_API_URL || 'http://localhost:8000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
     withCredentials: true,
 });
 
